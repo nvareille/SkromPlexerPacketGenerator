73dd511124d9240069d9a0d0e1d209d2be3a8041
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2017-05-16 15:39:18 +0200

    Basic tool to create PacketHandler

---
+++ SkromPlexerPacketGenerator/FileCreator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkromPlexer.PacketHandlers;
+
+namespace SkromPlexerPacketGenerator
+{
+    public class FileCreator
+    {
+        public string Name;
+        public List<PacketCreatorFunction> PCFcts;
+        
+        public FileCreator(string name)
+        {
+            Name = name;
+            PCFcts = new List<PacketCreatorFunction>();
+        }
+
+        public void CreatePacketHandler()
+        {
+            Directory.CreateDirectory("Output");
+
+            string str = String.Format("public class {0}PacketHandler : APacketHandler\n{{\n{1}\n}}", Name, BuildMethods());
+
+            Console.WriteLine("Writing " + Name + "...");
+            File.WriteAllText("Output/" + Name.Replace("PacketCreator", "PacketHandler") + ".cs", str);
+        }
+
+        public string BuildMethods()
+        {
+            string str = "";
+
+            foreach (PacketCreatorFunction fct in PCFcts)
+            {
+                str += String.Format("\tpublic static List<Packet> {0}(Core core, Client client, Packet packet)\n\t{{{1}\n\t}}\n\n", fct.Packet, GenArgs(fct));
+            }
+
+            return (str);
+        }
+
+        public string GenArgs(PacketCreatorFunction fct)
+        {
+            string str = "";
+
+            if (fct.Args == null)
+                return ("\n\t\treturn (null);");
+
+            str += "\n\t\tPacketArgs args = packet.GetArgs(new Type[]\n\t\t{\n";
+
+            foreach (Type arg in fct.Args)
+            {
+                string t = "";
+
+                if (arg == typeof(string))
+                    t = "string";
+                if (arg == typeof(int))
+                    t = "int";
+                if (arg == typeof(float))
+                    t = "float";
+                if (arg == typeof(bool))
+                    t = "bool";
+
+                str += "\t\t\ttypeof(" + t + "),\n";
+            }
+            str += "\t\t});\n\n";
+            str += "\t\treturn (null);";
+
+            return (str);
+        }
+    }
+}
---
+++ SkromPlexerPacketGenerator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SkromPlexer.PacketHandlers;
+
+namespace SkromPlexerPacketGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FileCreator> files = new List<FileCreator>();
+
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(args[0]);
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.GetCustomAttribute<PacketCreator>() != null)
+                    {
+                        FileCreator fc = new FileCreator(type.Name);
+
+                        files.Add(fc);
+                        Console.WriteLine("Extracting " + type.Name + ":\n");
+                        foreach (var method in type.GetMethods())
+                        {
+                            PacketCreatorFunction fct = method.GetCustomAttribute<PacketCreatorFunction>();
+
+                            if (fct != null)
+                            {
+                                fc.PCFcts.Add(fct);
+                                Console.WriteLine("\t" + fct.Packet);
+                            }
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+
+                foreach (FileCreator file in files)
+                {
+                    file.CreatePacketHandler();
+                }
+
+                Console.Write("Done !");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Impossible to generate the files, check the argument");
+                
+            }
+        }
+    }
+}
